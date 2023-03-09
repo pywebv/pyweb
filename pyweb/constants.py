668a4b48c9fddcd1b36e8c802cdb6b18a817995f
PyWebv <127393306+pywebv@users.noreply.github.com>
2023-03-09 22:45:53 +0530

    Create constants.py

---
+++ pyweb/constants.py
@@ -0,0 +1,23 @@
+# PyWeb - Python add-on extension to Pyrogram
+# Copyright (C) 2023-2024 PyWebV <https://github.com/pywebv>
+#
+# This file is part of PyWeb.
+#
+# PyWeb is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyWeb is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyWeb. If not, see <https://www.gnu.org/licenses/>.
+
+__version__ = "1.2.0"
+__description__ = "An add-on extension to Pyrogram for absolute beginners"
+__copyright__ = "Copyright (C) 2023-2024 PyWebV <https://github.com/pywebv>"
+__license__ = "GNU General Public License v3 or later (GPLv3+)"
+__terms__ = f"PyWeb is free software and comes with ABSOLUTELY NO WARRANTY. \nLicensed under the terms of {__license__}."
