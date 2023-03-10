41ab79ac442ba42af0b6a6df2b23889a2eac3a83
PyWebv <127393306+pywebv@users.noreply.github.com>
2023-03-10 12:01:31 +0530

    Create sudo.py

---
+++ pyweb/plugins/addons/sudo.py
@@ -0,0 +1,29 @@
+# PyWeb - Python add-on extension to Pyrogram
+# Copyright (C) 2023-2024 PyWebV <https://github.com/pywebv>
+#
+# This file is part of PyWeb.
+#
+# PyWeb is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyWeb is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyWeb. If not, see <https://www.gnu.org/licenses/>.
+
+from pyweb import Web, Message
+
+
+@Web.cmd("sudo", owner_only=True)
+async def sudo(bot: Web, msg: Message):
+    text = "Sudo Commands Available: \n"
+    num = 0
+    for i in bot.sudo_commands:
+        num += 1
+        text += f"\n{num}) /{i}"
+    await msg.react(text)
