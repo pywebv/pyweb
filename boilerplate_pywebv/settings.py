029e50b770ed429367bf074047955396ddd00c9b
PyWebv <127393306+pywebv@users.noreply.github.com>
2023-03-11 14:42:49 +0530

    Create settings.py

---
+++ boilerplate_pywebv/settings.py
@@ -0,0 +1,25 @@
+# Project Settings
+
+START = "do this"
+
+HELP = "Help Message"
+
+ABOUT = "do this"
+
+REPO = "pyweb"
+
+PYWEBV = True
+
+# FULL_MESSAGES = True
+
+# CUSTOM_USERS_TABLE = True
+
+# NO_BUTTONS = True
+
+# REMOVE_ADDONS = ["must_join"]
+
+# ADDONS = ["start", "help", "id", "about"]
+
+# DATABASE_TABLES = ["users"]
+
+# START_IN_GROUPS = "Hey :) ping me privately if you have any questions on how to use me."
